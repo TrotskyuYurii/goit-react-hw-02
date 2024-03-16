0e73bea86b41b64b2f78ab2a3251e1f09b71b76f
y.trotskiy <143190183+TrotskyuYurii@users.noreply.github.com>
2024-03-16 23:43:38 +0200

    visible

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -2,18 +2,25 @@
 
 const Feedback = ({cafeState, totalState, possitivePercent}) => {
 
+  if (totalState>0) {
+    return (
+      <div>
+          <ul>
+              <li>Good: {cafeState.good}</li>
+              <li>Neutral: {cafeState.neutral}</li>
+              <li>Bad: {cafeState.bad}</li>
+              <li>Total: {totalState}</li>
+              <li>Possitive: {possitivePercent}%</li>
+          </ul>
+      </div>
+    )
+  } else {
+    return (
+      <p>No feedback yet</p>
+    )
+  };
 
-  return (
-    <div>
-        <ul>
-            <li>Good: {cafeState.good}</li>
-            <li>Neutral: {cafeState.neutral}</li>
-            <li>Bad: {cafeState.bad}</li>
-            <li>Total: {totalState}</li>
-            <li>Possitive: {possitivePercent}%</li>
-        </ul>
-    </div>
-  )
+
 }
 
 export default Feedback
